0c3cab72174ee59544d9f5a9b94a57e471618a79
Nicklas Frahm <nicklas.frahm@gmail.com>
2022-05-31 23:56:13 +0200

    fix(ops): fix deduplication function

--- pkg/ops/deduplicate.go
+++ pkg/ops/deduplicate.go
@@ -1,54 +1,25 @@
 package ops
 
-// DeduplicateDevices merges two slices by retaining only all unique items.
-func DeduplicateDevices(existing []Device, current []Device) []Device {
-	unique := make(map[string]*Device, 0)
+import (
+	"github.com/nicklasfrahm/netadm/pkg/driver"
+)
+
+// DeduplicateDevices accepts a single slice and returns a new slice with all unique items.
+func DeduplicateDevices(duplicates []driver.Device) []driver.Device {
+	unique := make(map[string]*driver.Device, 0)
 
 	// Add existing objects to map and
 	// identify them by their MAC.
-	for _, object := range existing {
-		unique[object.MAC.String()] = &object
-	}
-
-	// Overwrite existing objects that
-	// have been updated by the current
-	// operation.
-	for _, object := range current {
-		unique[object.MAC.String()] = &object
+	for _, object := range duplicates {
+		unique[object.System.MAC.String()] = &object
 	}
 
 	// Convert map to slice.
-	objects := make([]Device, len(unique))
+	objects := make([]driver.Device, len(unique))
 	i := 0
 	for _, object := range unique {
 		objects[i] = *object
-	}
-
-	return objects
-}
-
-// DeduplicateMessages merges two slices by retaining only all unique items.
-func DeduplicateMessages(existing []Message, current []Message) []Message {
-	unique := make(map[string]*Message, 0)
-
-	// Add existing objects to map and
-	// identify them by their MAC.
-	for _, object := range existing {
-		unique[string(object.Header.ClientMAC[:])] = &object
-	}
-
-	// Overwrite existing objects that
-	// have been updated by the current
-	// operation.
-	for _, object := range current {
-		unique[string(object.Header.ClientMAC[:])] = &object
-	}
-
-	// Convert map to slice.
-	objects := make([]Message, len(unique))
-	i := 0
-	for _, object := range unique {
-		objects[i] = *object
+		i += 1
 	}
 
 	return objects
