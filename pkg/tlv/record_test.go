8f465d9adf29be3c1f3d7e93e6b89d833cb4b156
Nicklas Frahm <nicklas.frahm@gmail.com>
2022-06-01 20:08:05 +0200

    feat(tlv): add tests for Marshal function

--- pkg/tlv/record_test.go
+++ pkg/tlv/record_test.go
@@ -1,6 +1,7 @@
 package tlv
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,7 +24,7 @@ func TestUnmarshal(t *testing.T) {
 
 	// Assert.
 	assert.Nil(err, "should not return an error")
-	assert.Equal(2, len(*records), "should stop parsing after end of message record")
+	assert.Equal(2, len(*records), "should stop decoding after end of message record")
 	assert.Equal(RecordList{
 		{
 			Type:   0x0099,
@@ -35,5 +36,59 @@ func TestUnmarshal(t *testing.T) {
 			Length: 0x0002,
 			Value:  []byte{'h', 'i'},
 		},
-	}, *records, "should parse data and omit end of message record")
+	}, *records, "should decode data and omit end of message record")
+}
+
+func TestUnmarshalInvalidEndOfMessage(t *testing.T) {
+	assert := assert.New(t)
+
+	// Arrange.
+	data := []byte{
+		0x00, 0x99, 0x00, 0x04, 't', 'e', 's', 't',
+		0xFF, 0xFF, 0x11, 0x11,
+	}
+	records := new(RecordList)
+
+	// Act.
+	err := records.UnmarshalBinary(data)
+
+	// Assert.
+	assert.Equal(errors.New("invalid end of message"), err, "should return an error")
+	assert.Equal(1, len(*records), "should partially decode data")
+	assert.Equal(RecordList{
+		{
+			Type:   0x0099,
+			Length: 0x0004,
+			Value:  []byte{'t', 'e', 's', 't'},
+		},
+	}, *records, "should decode data until invalid end of message record")
+}
+
+func TestMarshal(t *testing.T) {
+	assert := assert.New(t)
+
+	// Arrange.
+	records := &RecordList{
+		{
+			Type:   0x0099,
+			Length: 0x0004,
+			Value:  []byte{'t', 'e', 's', 't'},
+		},
+		{
+			Type:   0x9900,
+			Length: 0x0002,
+			Value:  []byte{'h', 'i'},
+		},
+	}
+
+	// Act.
+	bytes, err := records.MarshalBinary()
+
+	// Assert.
+	assert.Nil(err, "should not return an error")
+	assert.Equal(14, len(bytes), "should encode the correct number of bytes")
+	assert.Equal([]byte{
+		0x00, 0x99, 0x00, 0x04, 't', 'e', 's', 't',
+		0x99, 0x00, 0x00, 0x02, 'h', 'i',
+	}, bytes, "should encode all data without including an end of message record")
 }
